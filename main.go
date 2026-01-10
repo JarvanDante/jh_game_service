63c045aa79ed073af1b2ade0bd3b3b7d0476162e
jarvenwang <wang_jin_bao@163.com>
2026-01-10 13:09:04 +0800

    [jh_game_service]修改时区

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 
 	_ "jh_game_service/internal/logic"
 
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 
 	"jh_game_service/internal/cmd"
 )
 
 func main() {
-	cmd.Main.Run(gctx.New())
+	// 在最早的时刻设置时区
+	ctx := gctx.New()
+	timezone := g.Cfg().MustGet(ctx, "timezone").String()
+	if timezone == "" {
+		timezone = "Asia/Shanghai"
+	}
+	loc, err := time.LoadLocation(timezone)
+	if err != nil {
+		fmt.Printf("加载时区失败: %v，使用默认时区\n", err)
+		loc, _ = time.LoadLocation("Asia/Shanghai")
+	}
+	time.Local = loc
+	fmt.Printf("时区设置: %s\n", timezone)
+
+	cmd.Main.Run(ctx)
 }
